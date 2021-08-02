b557607b709be42a969facc10ad103304570dbb8
venemap <venemap@rose-hulman.edu>
2021-08-02 19:54:13 -0400

    budget overview page reads from firestore, login functionality works, logout does not

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,27 +2,141 @@ var rhit = rhit || {};
 
 /** globals */
 rhit.variableName = "";
-rhit.fbAuthManger = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
+rhit.FB_BUDGET_COLLECTION = "Budget"
+rhit.FB_KEY_AMOUNT = "Amount";
+rhit.FB_KEY_CATEGORY = "Category";
+rhit.fbBudgetManager = null;
+rhit.fbSingleBudgetManager = null;
+rhit.fbAuthManager = null;
 
-rhit.ClassName = class {
-	constructor() {
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
 
-	}
-
-	methodName() {
-
+rhit.Budget = class {
+	constructor(id, category, amount) {
+		this.id = id;
+		this.category = category;
+		this.amount = amount;
 	}
 }
+
+rhit.BudgetListPageController = class {
+	constructor() {
+		rhit.fbBudgetManager.beginListening(this.updateList.bind(this));
+		
+		if(document.querySelector("#logoutClick")) {
+			document.querySelector("#logoutClick").onclick = (event) => {
+				console.log("sign out");
+				firebase.auth().signOut();
+	
+				// rhit.fbAuthManager.signOut();
+			}
+		}
+	}
+
+	_createBudget(budget) {
+		return htmlToElement(`
+		<div id="BudgetOverview-Label">
+			<span>${budget.category}: $ XXX/${budget.amount}</span>
+  
+			<span class="dropdown pull-xs-right budget-option-menu">
+				<button class="btn bmd-btn-icon dropdown-toggle" type="button" id="lr1" data-toggle="dropdown">
+					<i class="material-icons">more_horiz</i>
+				</button>
+				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="lr1">
+					<button class="dropdown-item" type="button"><i class="material-icons">edit</i>&nbsp;&nbsp;&nbsp;&nbsp;Edit</button>
+					<button class="dropdown-item" type="button"><i class="material-icons">delete</i>&nbsp;&nbsp;&nbsp;&nbsp;Delete</button>
+				</div>
+			</span>
+	  	</div>
+	  <br>`)
+	}
+	
+	updateList() {
+		console.log("update the page for budgets");
+
+		console.log(`Num budgets = ${rhit.fbBudgetManager.length}`);
+		console.log(`Example Budget: `, rhit.fbBudgetManager.getBudgetAtIndex(0));
+
+		const newList = htmlToElement('<div id="BudgetOverview-Label"></div>')
+
+		for (let i = 0; i < rhit.fbBudgetManager.length; i++) {
+			const budget = rhit.fbBudgetManager.getBudgetAtIndex(i);
+
+			const newBudget = this._createBudget(budget);
+
+			newBudget.onclick = (event) => {
+				console.log(`You clicked on ${budget.id}`);
+
+				// window.location.href = "/budget"
+			}
+
+			newList.appendChild(newBudget);
+
+			
+		}
+		const oldBudget = document.querySelector("#BudgetOverview-Label");
+		oldBudget.removeAttribute("id");
+
+		oldBudget.hidden = true;
+		oldBudget.parentElement.appendChild(newList);
+
+	}
+
+
+}
+
+rhit.FbBudgetManager = class {
+	constructor() {
+		this._documentSnapshots = [];
+		this._ref = firebase.firestore().collection(rhit.FB_BUDGET_COLLECTION);
+		this._unsubscribe = null;
+	}
+
+	beginListening(changeListener) {
+		this._unsubscribe = this._ref.limit(30).onSnapshot((querySnapshot) => {
+			console.log("Budget page Update");
+
+			this._documentSnapshots = querySnapshot.docs;
+
+			querySnapshot.forEach((doc) => {
+				console.log(doc.data);
+			})
+
+			changeListener();
+		})
+	}
+
+	stopListening() {
+		this._unsubscribe();
+	}
+
+	get length  () {
+		return this._documentSnapshots.length;
+	}
+
+	getBudgetAtIndex(index) {
+		const docSnapshot = this._documentSnapshots[index];
+		const budget = new rhit.Budget(
+			docSnapshot.id, 
+			docSnapshot.get(rhit.FB_KEY_CATEGORY),
+			docSnapshot.get(rhit.FB_KEY_AMOUNT),
+		);
+		return budget;
+	}
+}
+
+
 
 rhit.LoginPageController = class {
 	constructor() {
 		document.querySelector("#loginBtn").onclick = (event) => {
-			rhit.fbAuthManger.signIn();
+			rhit.fbAuthManager.signIn();
 		};
 	}
 }
@@ -34,13 +148,14 @@ rhit.AuthManager = class {
 
 	beginListening(changeListener) {
 		firebase.auth().onAuthStateChanged((user) => {
+			console.log("auth state changing");
 			this._user = user;
 			changeListener();
 		});
 	}
 
 	signIn() {
-		Rosefire.signIn("0add59d9-0ffe-47b7-b3c3-4c40a8eaec62", (err, rfUser) => {
+		Rosefire.signIn("70e641ae-bd99-4014-b214-e680668ac0cc", (err, rfUser) => {
 			if (err) {
 				console.log("Rosefire error!", err);
 				return;
@@ -76,11 +191,13 @@ rhit.AuthManager = class {
 }
 
 rhit.checkForRedirects = function(){
-	if(document.querySelector("#login") && rhit.fbAuthManger.isSignedIn){
+	console.log("checking for redirects");
+	if(document.querySelector("#login") && rhit.fbAuthManager.isSignedIn){
 		window.location.href = "/main.html";
 	}
 
-	if(!document.querySelector("#login") && !rhit.fbAuthManger.isSignedIn){
+	if(!document.querySelector("#login") && !rhit.fbAuthManager.isSignedIn){
+		console.log("not logged in and on main site");
 		window.location.href = "/";
 	}
 
@@ -104,10 +221,24 @@ rhit.initializePage = function(){
 rhit.main = function () {
 	console.log("Ready");
 
-	rhit.fbAuthManger = new rhit.AuthManager();
-	rhit.fbAuthManger.beginListening((event) => {
+	if(document.querySelector("#logoutClick")) {
+		document.querySelector("#logoutClick").onclick = (event) => {
+			console.log("sign out");
+			rhit.fbAuthManager.signOut();
+		}
+	}
+
+
+	console.log("making new authmanager");
+	rhit.fbAuthManager = new rhit.AuthManager();
+	rhit.fbAuthManager.beginListening((event) => {
 		console.log("We are listening now. Using AuthManager.");
-		console.log("isSigned: ", rhit.fbAuthManger.isSignedIn);
+		console.log("isSigned: ", rhit.fbAuthManager.isSignedIn);
+
+		if(document.querySelector(".sidebar")){
+			console.log("sidebar is presetn");
+			document.querySelector("#greetUser").innerHTML = `Hello, ${rhit.fbAuthManager.uid}`;
+		}	
 
 		rhit.checkForRedirects();
 
@@ -131,6 +262,16 @@ rhit.main = function () {
 		};
 
 	}
+
+	if(document.querySelector("#budgetOverviewPage")) {
+		console.log("you are on the budget list page");
+
+		rhit.fbBudgetManager = new rhit.FbBudgetManager();
+
+		new rhit.BudgetListPageController();
+	}
+
+	
 
 
 
